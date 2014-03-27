5e4b89ba20cbc453c1a80ad048a9f0660e2339c1
Sébastien Piquemal <sebpiq@gmail.com>
2014-03-27 12:30:13 +0200

    added affix menu. Needs styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// =============== OS selection =============== //
 $(function(){
 	// switch to client OS automatically
 	var os = $.client.os;
@@ -34,3 +35,38 @@ var showOS = function(os) {
 	$(selector).show();
 }
 
+
+// =============== Bootstrap affix plugin =============== //
+$(function() {
+	// Populates the affix with h2 and h3 titles from the page
+	var affixUl = $('#affix ul')
+		, previousH2 = null
+
+	var setupTitle = function(titleElem, parentElem, titleHash) {
+		var titleText = titleElem.text()
+			, titleMenuItem = $('<li>')
+			, titleAnchor = $('<a>', { href: '#' + titleHash, class: 'titleAnchor' }).html(' #')
+		titleElem.attr('id', titleHash)
+		titleElem.append(titleAnchor)
+		titleMenuItem.append($('<a>', { href: '#' + titleHash }).html(titleText))
+		parentElem.append(titleMenuItem)
+		return titleMenuItem
+	}
+
+	// Iterate on all h2 and set-up title
+	$('h2').each(function(i, h2) {
+		h2 = $(h2)
+		var h2Hash = 'titles.' + (i + 1)
+			, h2MenuItem = setupTitle(h2, affixUl, h2Hash)
+			, h2MenuChildList = $('<ul>')
+		h2MenuItem.append(h2MenuChildList)
+		h2MenuItem.addClass('affixH2Item affixItem')
+
+		// Iterate on all h3 that are children of the current h2, and set-up title
+		h2.nextUntil('h2').filter('h3').each(function(i, h3) {
+			h3 = $(h3)
+			var h3MenuItem = setupTitle(h3, h2MenuChildList, h2Hash + '.' + (i + 1))
+			h3MenuItem.addClass('affixH3Item affixItem')
+		})
+	})
+})
